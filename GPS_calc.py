b110f4fb386af917d6cdce83f99b35515d3be790
Thomas-David Griedel <thomas-david.griedel2@htw-dresden.de>
2021-06-22 17:35:57 +0200

    Update GPS_calc.py

--- GPS_calc.py
+++ GPS_calc.py
@@ -8,13 +8,19 @@ def roughly_equal(a,b):
     #TODO: SMARTER LOGIC
     return a==b
 
-#CALCULATION
-def add_offset_lnglat(lng, lat, ang, os):
-    lat0 = math.cos(math.PI / 180.0 * lat)
-    lng_new = lng + (180/math.PI) * (os / 6378137)/math.cos(lat0) * math.cos(ang)
-    lat_new = lat + (180/math.PI) * (os / 6378137) * math.sin(angle)
+def add_offset(GPS_obj, ang, os):
 
-#cable length
+    #CALCULATION
+    lat0 = math.cos(math.PI / 180.0 * lat)
+    lng_new = GPS_obj.longitude + (180/math.PI) * (os / 6378137)/math.cos(lat0) * math.cos(ang)
+    lat_new = GPS_obj.latitude + (180/math.PI) * (os / 6378137) * math.sin(angle)
+
+    #TODO:GENERATE & RETURN NEW GPS OBJECT                   
+    GPS_new_data = pynmea2.GGA('GP', 'GGA', ('184353.07', '1929.045', 'S', '02410.506', 'E', '1', '04', '2.6', '100.00', 'M', '-33.9', 'M', '', '0000'))
+
+    return GPS_new_data
+
+#cable length TODO: MAKE IT AN ARGUMENT
 string_length = 2.0 
 
 # Start MavProxy
@@ -26,9 +32,8 @@ time.sleep(5)
 # Setup GPS serial port
 port_GPS = "/dev/serial0"
 ser = serial.Serial(port_GPS,baudrate=9600,timeout=0.5)
-dataout = pynmea2.NMEAStreamReader()
 
-# open socket to Boot-pi
+# Open socket to Boot-pi
 BOOT_IP = "192.168.2.2"
 UDP_PORT = 27000
 sock = socket.socket(socket.AF_INET, # Internet
@@ -40,27 +45,34 @@ connection_boje = '127.0.0.1:14550'
 connection_boot = '192.168.2.2:14550'
 boje = connect(connection_boje, wait_ready=True)
 boot = connect(connection_boot, wait_ready=True)
-
-try:
-    while True:
+while True:
+    try:
         depth = boot.location.global_relative_frame.alt
-        offset = sqrt((string**2)-(depth**2))
+        offset = sqrt((string**2)-(depth**2))       
         compass_boot = boot.heading
         compass_boje = boje.heading
         speed_boot = boot.groundspeed
         speed_boje = boje.groundspeed
-        GPS_obj = pynmea2.parse(ser.readline())
+        GPS_boje_data = pynmea2.parse(ser.readline())
         angle = math.toRadians(50)                 #TODO: calculate angle from compasses
         
         if (roughly_equal(speed_boje, speed_boot)) :
-            #TODO: find a way to generate NMEA
-            add_offset_lnglat(GPS_obj.longitude, GPS_obj.latitude, angle, offset)
-        
-        bytestosend = bytes(newmsg)
-        sock.sendto(bytestosend, (BOOT_IP, UDP_PORT))
-except KeyboardInterrupt:
-    #ctrl+c cleanup
-    server_proc.kill()
+            GPS_boot_new = add_offset(GPS_boje_data, angle, offset)
+            bytestosend = bytes(newmsg)
+            sock.sendto(bytestosend, (BOOT_IP, UDP_PORT))
+
+    except serial.SerialException as e:
+        print('Device error: {}'.format(e))
+        break
+    except pynmea2.ParseError as e:
+        print('Parse error: {}'.format(e))
+        continue
+    except serial.error:
+        print("Couldnt send to the socket-server")
+        continue
+    except KeyboardInterrupt:
+        #ctrl+c cleanup
+        server_proc.kill()
 
 
     # Display basic boje state
