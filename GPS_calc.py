1957310a117ee0f20a55cf1edba601db753338f9
Thomas-David Griedel <thomas-david.griedel2@htw-dresden.de>
2021-07-19 16:51:46 +0200

    Update GPS_calc.py

--- GPS_calc.py
+++ GPS_calc.py
@@ -95,7 +95,7 @@ boje.wait_ready(True, timeout=180)
 
 while True:
     time.sleep(2)
-    print(str(boje.location.global_frame.lat))
+    print("lat: "+str(boje.location.global_frame.latitude))
     
 #    try:
 #        depth = boot.location.global_relative_frame.alt
