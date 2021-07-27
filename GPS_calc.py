4f18ba9f881424f473ede8e7a84958c3c9df3176
Thomas-David Griedel <thomas-david.griedel2@htw-dresden.de>
2021-07-27 18:49:21 +0200

    Update GPS_calc.py

--- GPS_calc.py
+++ GPS_calc.py
@@ -110,7 +110,7 @@ print("mavlink messages:")
 @boje.on_message('GPS_RAW_INT')
 def listener(self, name, message):
     utc = datetime(1970, 1, 1) + timedelta(seconds=message.time_usec)
-    gps_timestamp = str(message.time_usec)
+    gps_timestamp = str(message.time_usec/1000)
     #print(utc.strftime("%H%M%S"))
 
 while True:
