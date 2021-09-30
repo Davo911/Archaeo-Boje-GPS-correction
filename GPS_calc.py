6966e697911f12ce78bad5bb9a5c6811e4487746
Thomas-David Griedel <thomas-david.griedel2@htw-dresden.de>
2021-09-30 16:50:51 +0200

    Update GPS_calc.py

--- GPS_calc.py
+++ GPS_calc.py
@@ -130,7 +130,7 @@ while True:
 
     GPS_boje_data = pynmea2.parse("$GPRMC,150559.00,A,5102.85348,N,01345.01389,E,0.212,,300421,,,A*7C")
     #print(str(GPS_boje_data))
-    #print("sending ")
+    print("sending ...")
     #sock_boot.sendto(bytes(str(GPS_boje_data)), (BOOT_IP, BOOT_PORT))
     sock_gc.sendto(bytes(str(GPS_boje_data)), (GC_IP, GC_PORT))
 
