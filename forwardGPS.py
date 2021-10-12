126c35a3f4d26d650bf9824af9e5d6fa34370781
Thomas-David Griedel <T_D_Griedel@gmx.de>
2021-10-12 19:23:38 +0200

    Update forwardGPS.py

--- forwardGPS.py
+++ forwardGPS.py
@@ -22,11 +22,11 @@ boje = connect('localhost:14550',"baud=57600")#, wait_ready=False)
 #boje.wait_ready(True, timeout=180)
 
 while True:
-    time.sleep(0.2)
+    time.sleep(0.1)
     lat_dir = 'N' if boje.location.global_frame.lat > 0 else 'S'
     lon_dir = 'E' if boje.location.global_frame.lon > 0 else 'W'
 
-    GPS_boje = pynmea2.GGA('GP', 'GGA', (str(int(time.time())) , decTodms(boje.location.global_frame.lat), lat_dir,decTodms(boje.location.global_frame.lon), lon_dir, str(boje.gps_0.fix_type), str(boje.gps_0.satellites_visible), str(boje.gps_0.eph), str(boje.location.global_frame.alt), 'M', '0.0', 'M', '', '0000'))
+    GPS_boje = pynmea2.GGA('GP', 'GGA', (str(datetime.utcnow().timestamp()) , decTodms(boje.location.global_frame.lat), lat_dir,decTodms(boje.location.global_frame.lon), lon_dir, str(boje.gps_0.fix_type), str(boje.gps_0.satellites_visible), str(boje.gps_0.eph), str(boje.location.global_frame.alt), 'M', '0.0', 'M', '', '0000'))
     
     print(str(GPS_boje))
     sock_boot.sendto(bytes(str(GPS_boje)), (BOOT_IP, BOOT_PORT))
