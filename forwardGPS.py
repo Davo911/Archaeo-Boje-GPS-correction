3782cdd003c00d7d8c586c004b6f86817ce1796b
Thomas-David Griedel <T_D_Griedel@gmx.de>
2021-10-12 22:46:03 +0200

    Update forwardGPS.py

--- forwardGPS.py
+++ forwardGPS.py
@@ -26,7 +26,7 @@ while True:
     lat_dir = 'N' if boje.location.global_frame.lat > 0 else 'S'
     lon_dir = 'E' if boje.location.global_frame.lon > 0 else 'W'
 
-    GPS_boje = pynmea2.GGA('GP', 'GGA', (str(time.time()) , decTodms(boje.location.global_frame.lat), lat_dir,decTodms(boje.location.global_frame.lon), lon_dir, str(boje.gps_0.fix_type), str(boje.gps_0.satellites_visible), str(boje.gps_0.eph), '0', 'M', '0.0', 'M', '', '0000'))
+    GPS_boje = pynmea2.GGA('GP', 'GGA', ('203639.01' , decTodms(boje.location.global_frame.lat), lat_dir,decTodms(boje.location.global_frame.lon), lon_dir, str(boje.gps_0.fix_type), str(boje.gps_0.satellites_visible), str(boje.gps_0.eph), '0', 'M', '0.0', 'M', '', '0000'))
 
 #$GPGGA,203639.01,5555.2846,N,1133.1222,E,1,8,0,20,M,0,M,,*7F
 #$GPGSA,A,3,17,15,19,24,32,10,12,25,,,,,1.77,1.00,1.46*09
